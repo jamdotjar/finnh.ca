c15d052e71cfc9999c34017ace8ce63050e72e9b
jam.jar <darkestknight3d@outlook.com>
2025-12-14 21:49:47 -0800

    project grid?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 // https://astro.build/config
+
 export default defineConfig({
     site: 'https://finnh.ca',
     image: {
         responsiveStyles: true
+    },
+    experimental: {
+        fonts: [
+            {
+                name: "Switzer",
+                cssVariable: "--font-switzer",
+                provider: fontProviders.fontshare(),
+                weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+            },
+            {
+                name: "Manrope",
+                cssVariable: "--font-manrope",
+                provider: fontProviders.google(),
+                weights: [200, 300, 400, 500, 600, 700, 800, 900],
+
+            },
+            {
+                name: "Ibm Plex Mono",
+                cssVariable: "--font-ibmplexmono",
+                provider: fontProviders.google(),
+            }
+
+        ]
     }
 });
